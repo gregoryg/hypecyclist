461ae2ec901088ae83f744481e7b489dc309bb33
Gregory Grubbs <gregory@dynapse.com>
2020-04-17 07:53:41 -0600

    Finalize publish blog script

--- bin/publish-blog.sh
+++ bin/publish-blog.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-hugo && rsync --dry-run -av --delete  public/ gortsleigh@dynapse.net:hypecyclist.org/
+hugo && rsync  -av --delete  public/ gortsleigh@dynapse.net:hypecyclist.org/
